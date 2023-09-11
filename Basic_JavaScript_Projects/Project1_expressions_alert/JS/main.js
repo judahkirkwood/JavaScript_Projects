344f710c9694b48d540c0b06f30d28ce1e3efc74
judahkirkwood <judahkirkwood@gmail.com>
2023-09-11 01:35:59 -0700

    JavaScript Basics
    New JavaScript

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+// Pop-Up.//
+window.alert("Hello, world!");
+
+var J = ("What\'s up pal?" 
++  "What? That\'s not what you were expecting?");
+
+var Family = "Kirkwoods", Dad = "Duane", Mom = "Sandi", Brother = "Gabe", Sister = "Tab", Brother2 = "Jesse";
+// Showing Text //
+document.write(Brother2);
+
+document.write(3 * 3);
