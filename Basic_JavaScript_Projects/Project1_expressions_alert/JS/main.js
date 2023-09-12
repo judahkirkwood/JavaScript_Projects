0e4e2b158edea55877b7571b530f961aa3ca0206
judahkirkwood <judahkirkwood@gmail.com>
2023-09-11 21:59:06 -0700

    Fixes
    Updates and fixes to index.html and main.js files.

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,11 +1,36 @@
 // Pop-Up.//
 window.alert("Hello, world!");
 
-var J = ("What\'s up pal?" 
-+  "What? That\'s not what you were expecting?");
+var sent1 = "This is the beginning of the string";
+var sent2 = "and this is the end of the string";
+var result = sent1 + sent2;
+document.write(result);
+
+function My_First_Function() {
+    // Defining a function and naming it
+    var str = "This is the button text!"; // Defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str; // Putting the value of the variable into the HTML element with the "Button_Text" id
+}
+
+// Call your function to execute it and display the text
+My_First_Function();
+
 
 var Family = "Kirkwoods", Dad = "Duane", Mom = "Sandi", Brother = "Gabe", Sister = "Tab", Brother2 = "Jesse";
 // Showing Text //
-document.write(Brother2);
+document.write();
 
-document.write(3 * 3);
+document.write();
+
+const buttonElement = document.getElementById("myButton")
+
+buttonElement.addEventListener("mouseover", function() {
+    buttonElement.style.backgroundColor = "light-blue";
+});
+
+buttonElement.addEventListener("mouseout", function() {
+    buttonElement.style.backgroundColor = "";
+});
+
+var sent1 = "This is the first half";
+
---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+var (sent1) = "This is the first half";
+
+function My_First_Function() {
+    var str = "This is the button text!"
+
+    document.getElementById("Button_Text").innerHTML = str;
+}
