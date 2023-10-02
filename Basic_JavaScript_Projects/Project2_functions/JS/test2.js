4c3659f94fe105fdac05cae34427ce9084cfa512
judahkirkwood <judahkirkwood@gmail.com>
2023-10-02 15:15:28 -0700

    update
    more updates

--- Basic_JavaScript_Projects/Project2_functions/JS/test2.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/test2.js
@@ -44,3 +44,10 @@ function count_To_Ten() {
     }
     document.getElementById("Counting_to_Ten").innerHTML = Digit;
 }
+
+var ages = [2, 10, 18, 20, 13, 15, 30];
+checkAge = (age) => age >= 18;
+
+function myFunction2() {
+    document.getElementById('displayAge').innerHTML = ages.some(checkAge);
+}
